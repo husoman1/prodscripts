81b158b856e96a4364dd89fd1b34e22adafa0cf5
husoman1 <toremgames@hotmail.com>
2025-03-28 02:23:17 +0300

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,34 @@
 import "@/styles/globals.css";
-import { UserProvider } from "@/context/UserContext";
+import { UserProvider, useUser } from "@/context/UserContext";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+function AuthGuard({ children }) {
+  const router = useRouter();
+  const { user, loading } = useUser();
+
+  // Giriş gerektiren sayfalar
+  const protectedRoutes = ["/", "/premium"];
+
+  useEffect(() => {
+    const isProtected = protectedRoutes.includes(router.pathname);
+
+    if (!loading && isProtected && !user) {
+      router.push("/login");
+    }
+  }, [user, loading, router]);
+
+  if (loading) return <div className="p-4 text-center">Yükleniyor...</div>;
+
+  return children;
+}
 
 export default function App({ Component, pageProps }) {
   return (
     <UserProvider>
-      <Component {...pageProps} />
+      <AuthGuard>
+        <Component {...pageProps} />
+      </AuthGuard>
     </UserProvider>
   );
 }
