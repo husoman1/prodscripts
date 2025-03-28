a3c2b305e078f87fffc3d27c6d1f1a42f71ec6f0
husoman1 <toremgames@hotmail.com>
2025-03-28 20:07:56 +0300

    feat: email/password login eklendi

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
-// next-sitemap.config.js
 module.exports = {
     siteUrl: 'https://prodscript.com',
     generateRobotsTxt: true,
+    generateIndexSitemap: true, // 👈 bu satır ÖNEMLİ
     sitemapSize: 5000,
   };
   
